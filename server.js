10c6f32a391fd1851ee32cb399c89be8e8115fbe
rcornacchia <rlc2160@columbia.edu>
2016-06-30 20:04:14 -0400

    added authentication route

--- server.js
+++ server.js
@@ -43,7 +43,36 @@ app.get('/setup', function(req, res) {
 // API Routes
 // get an instance of the router for api routes
 var apiRoutes = express.Router();
-// TODO: route to authenticate a user
+// route to authenticate a user
+apiRoutes.post('/authenticate', function(req, res) {
+    // find the user
+    User.findOne({
+        name: req.body.name
+    }, function(err, user) {
+        if(err) throw err;
+        if(!user) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
+            // check if password matches
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
+                // if user is found and password is right => create token
+                var token = jwt.sign(user, app.get('superSecret'), {
+                    expiresIn: 1440 // expires in 24 hours
+                });
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+        }
+    });
+});
+
 // TODO: route middleware to verify a token
 
 // route to show a random message (GET http://localhost:8080/api/)
